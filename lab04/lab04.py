df4e76ad5142208f8a1b38df3044e51542273c61
Sasheanna <ssgroner@gmail.com>
2025-02-08 22:01:49 -0500

    Added instrucions for agent in lab04_trader_chat, got 5/10 test cases to succeed.

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -20,8 +20,14 @@ def run_console_chat(sign, **kwargs):
                 print('Ending match:', ending_match)
             break
 
-lab04_params = {}
+lab04_params = {
+    "template_file": "lab04/lab04_trader_chat.json",
+    "sign": "Samantha Groner",
+    "end_regex": r"TRADE COMPLETED (.*)"
+}
 
 if __name__ ==  '__main__':
     # run lab04.py to test your template interactively
+    trader_template_file = 'lab04/lab04_trader_chat.json'
+    run_console_chat(**lab04_params)
     pass
