27506308f64a0b2551c34d4a9f57b3e88d8202ad
Sasheanna <ssgroner@gmail.com>
2025-02-18 12:00:46 -0500

    Lab 05 submission

--- lab05/lab05.py
+++ lab05/lab05.py
@@ -25,6 +25,21 @@ def roll_for(skill, dc, player):
 def process_response(self, response):
     # Fill out this function to process the response from the LLM
     # and make the function call 
+    
+    # response is what we've gotten back from the agent (ollama)
+
+    # at some point we want to do this to check if it's trying to do a tool call
+    print(response.message.tool_calls)
+    # When you need to, you can use the 'roll_for' tool/function when you want the player to pass a skill check for something that they want to accomplish in the game. You will decide what activities need a skill check. You may not need to do skill checks for trivial things that a player may want to do, but the player may ask for a skill check. You will use 'roll_for' tool to check if the user passes a skill check. Only use tools if a skill check is needed.
+    if response.message.tool_calls:
+        self.messages.append({'role': 'tool',
+                            'name': response.message.tool_calls[0].function.name, 
+                            'arguments': response.message.tool_calls[0].function.arguments,
+                            'content': process_function_call(response.message.tool_calls[0].function)
+                            })
+        response = self.completion()
+    
+
     return response
 
 run_console_chat(template_file='lab05/lab05_dice_template.json',
